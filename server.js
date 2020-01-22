e0a991d093d83e2816e9e27b88a341441341163a
Noodles <charles.pitek@gmail.com>
2020-01-22 13:49:56 -0700

    created paths for tables.html and reserve.html

--- server.js
+++ server.js
@@ -7,7 +7,17 @@ var PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 
+var tables = [];
+var reservations = [];
+
 app.get("/", function(req, res){
     res.sendFile(path.join(__dirname, "index.html"));
 });
 
+app.get("/tables", function(req, res){
+    res.sendFile(path.join(__dirname, "tables.html"));
+});
+
+app.get("/reserve", function(req, res){
+    res.sendFile(path.join(__dirname, "reserve.html"));
+});
